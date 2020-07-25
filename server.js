2f5173a192852e7f05bd4bc7649bde1060d2881c
resparzasoto <rubenesparzasoto95@gmail.com>
2020-07-25 14:33:23 -0600

    ¿Cómo pueden venir las peticiones?

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const router = express.Router();
 
 var app = express();
 
-app.use('/', (req, res) => res.send('Hola Mundo'));
+app.use(router);
+
+router.get('/message', (req, res) => res.send('Lista de Mensajes'));
+
+router.post('/message', (req, res) => res.send('Mensaje añadido'));
+
+router.delete('/message', (req, res) => res.send('Mensaje eliminado'));
+
+// app.use('/', (req, res) => res.send('Hola Mundo'));
 
 app.listen(3000);
-console.log('La aplicacion esta escuchando en http://localhost:3000');
+console.log('La aplicación está escuchando en http://localhost:3000');
