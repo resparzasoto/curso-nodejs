0b9e5ec8f229e2c7d349ab370739b870b70c97e8
resparzasoto <rubenesparzasoto95@gmail.com>
2020-07-29 22:00:37 -0600

    MongoDB: Almacenar y leer datos

--- components/message/store.js
+++ components/message/store.js
@@ -1,11 +1,28 @@
-const list = [];
+const mongoose = require('mongoose');
+const Model = require('./model');
+const chalk = require('chalk');
+
+const secrets = require('../../secrets.json');
+const connection = secrets.connectionStrings.find((connection) => connection.name === 'telegrom');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(connection.connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.once('open', console.log.bind(console, chalk.green('[db] Conectada con éxito')));
+db.on('error', console.error.bind(console, chalk.red('[db] Error de conexión')));
 
 const addMessage = (message) => {
-    list.push(message);
+    const myMessage = new Model(message);
+    myMessage.save();
 }
 
-const getMessages = () => {
-    return list;
+const getMessages = async () => {
+    const messages = await Model.find();
+    return messages;
 }
 
 module.exports = {
